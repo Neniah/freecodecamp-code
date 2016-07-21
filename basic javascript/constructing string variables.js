fe8e99e23a44eed8ea0e5cb7f3fcee55e942e6a8
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-07-21 12:15:57 +0200

    string variables

---
+++ basic javascript/constructing string variables.js
@@ -0,0 +1,8 @@
+
+// Example
+var ourName = "Free Code Camp";
+var ourStr = "Hello, our name is " + ourName + ", how are you?";
+
+// Only change code below this line
+var myName = "Maria";
+var myStr = "Hello, my name is " + myName + ", how are you?";
