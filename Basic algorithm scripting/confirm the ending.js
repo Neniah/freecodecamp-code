e443c1a206ad93e629a3a3ad121e4a43352f169d
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-07-25 16:16:12 +0200

    confirm the ending

---
+++ Basic algorithm scripting/confirm the ending.js
@@ -0,0 +1,12 @@
+function confirmEnding(str, target) {
+  // "Never give up and good luck will find you."
+  // -- Falcor
+  if (str[str.length-1] == target){
+    return true;
+  } else{
+    return false;
+  }
+  return str;
+}
+
+confirmEnding("Bastian", "n");
