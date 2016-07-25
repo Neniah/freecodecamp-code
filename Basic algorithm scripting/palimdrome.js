590a9f5630a98411d80d14e4ef9ccdf12870b406
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-07-25 10:24:24 +0200

    palindrome

---
+++ Basic algorithm scripting/palimdrome.js
@@ -0,0 +1,12 @@
+
+function palindrome(str) {
+  // Good luck!
+
+  str = str.replace(/[\W_]/g , "").toLowerCase();
+
+  return str === str.split('').reverse().join('');
+}
+
+
+
+palindrome("eye");
