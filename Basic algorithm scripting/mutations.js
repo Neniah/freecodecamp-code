58fc6e755ffd72dbfcdaffca64fdb2bbc7656f73
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-07-29 16:52:45 +0200

    party on!

--- Basic algorithm scripting/mutations.js
+++ Basic algorithm scripting/mutations.js
@@ -1,18 +1,15 @@
+
 function mutation(arr) {
   var first = arr[0].toLowerCase();
   var second = arr[1].toLowerCase();
 
-  var count = 0;
-  for(var i = 0; i < second.length; i++){
-    for(var j = 0; j < first.length; j++){
-      if( second[i] == first[j])
-        count++;
-    }
-  }
-  if(count == second.length)
-  {  return true;}
-  else
-   { return false;}
+ for( var i = 0; i < second.length; i++){
+	res = first.indexOf(second[i]);
+   if(res === -1){
+     return false;
+   }
+ }
+  return true;
 }
 
 mutation(["hello", "Hello"]);
