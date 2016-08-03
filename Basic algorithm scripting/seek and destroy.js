260457a7d571252d4afb1bb11bf1ce94985b140c
Maria Lobillo <maria.lobillo.santos@gmail.com>
2016-08-03 20:24:12 +0200

    result

--- Basic algorithm scripting/seek and destroy.js
+++ Basic algorithm scripting/seek and destroy.js
@@ -1,7 +1,9 @@
 
 function destroyer(arr) {
   // Remove all the values
-  return arr;
+  args = Array.prototype .slice.call(arguments,1);
+  function remove(x) { return args.indexOf(x) === -1; }
+  return arguments[0].filter(remove)
 }
 
 destroyer([1, 2, 3, 1, 2, 3], 2, 3);
